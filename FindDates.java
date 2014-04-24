d42a500090c2bba9714e70ce73bedebaeb4f7bd8
ranlevinstein <ran1@me.com>
2014-04-25 01:22:14 +0300

    Update FindDates.java

--- FindDates.java
+++ FindDates.java
@@ -2,8 +2,8 @@ public class main {
 	public static void main(String []args)
     {
         String input = "As of May 2013, Apple maintains 408 retail stores";
-        input = input.replace('.', ' ');
-        input = input.replace(',', ' ');
+        input = input.replace(".", "  .");
+        input = input.replace(",", " ,");
         String[] splited = input.split(" ");
         for(int i=0;i<splited.length;i++)
         {
