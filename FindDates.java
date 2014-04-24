a2d9cb9754cb72f5cc96229c898c77571cb560d6
ranlevinstein <ran1@me.com>
2014-04-25 00:45:06 +0300

    Update FindDates.java

--- FindDates.java
+++ FindDates.java
@@ -1,1 +1,28 @@
-1
+
+/**
+ * Write a description of class FindDates here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class FindDates
+{
+    // instance variables - replace the example below with your own
+
+    public FindDates()
+    {
+        // initialise instance variables
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public static void Main()
+    {
+        // put your code here
+        
+    }
+}
