ab238278cd321d129c4aa9ceab8156ff87ae2688
ran levinstein <ran1@me.com>
2014-04-24 23:03:20 +0300

    Class for sorting data out of html source

---
+++ HtmlOrdering.java
@@ -0,0 +1,26 @@
+/**
+ * Write a description of class HtmlOrdering here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class HtmlOrdering
+{
+    // instance variables - replace the example below with your own
+        String htmlSource;
+    /**
+     * Constructor for objects of class HtmlOrdering
+     */
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public String findText ()
+    {
+        // put your code here
+        return x + y;
+    }
+}
