bd0de86a71482853c40c9127a66c5b4112e62341
ranlevinstein <ran1@me.com>
2014-04-25 01:33:12 +0300

    Update FindDates.java

--- FindDates.java
+++ FindDates.java
@@ -2,21 +2,27 @@ public class main {
 	public static void main(String []args)
     {
         String input = "As of May 2013, Apple maintains 408 retail stores";
+        
         input = input.replace(".", "  .");
         input = input.replace(",", " ,");
-        String[] splited = input.split(" ");
-        for(int i=0;i<splited.length;i++)
+        String[] words = input.split(" ");
+        String[] months = {"January","Febuary","March","April","May","June","July","August","September","October","November","December"};
+        for(int i=1;i<words.length;i++)
         {
             try
             { 
-                int a = Integer.parseInt(splited[i]);
-                System.out.println(a);
+                int a = Integer.parseInt(words[i]);
+                for(int j = 0; j < months.length; j++){
+                    if (words[i-1] == months[j]){
+                        System.out.println(a);
+                    }
+                }
+                
+            
             }catch(NumberFormatException nfe)
             {
    
             }
-            
-   
         }
         System.out.println(input);
     }
