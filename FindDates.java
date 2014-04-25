588e35cc64eb18b60c7168035bfbe0a601ddb114
nirlic <nir_lichtman@hotmail.com>
2014-04-25 12:31:01 +0300

    Added save dates and description

--- FindDates.java
+++ FindDates.java
@@ -1,11 +1,20 @@
-public static void main(String []args)
+import java.util.LinkedList;
+import java.util.List;
+
+public class main {
+	public static void main(String []args)
     {
-        String input = "As of May 2013, Apple maintains 408 retail stores";
+		
+		List<String> timeline  = new LinkedList<>();
+		List<String> meanings = new LinkedList<>();
+        String input = "As of May 2013, Apple maintains 408 retail stores. Bla Bla Bla";
         
-        input = input.replace(".", "  .");
+        input = input.replace(".", " .");
         input = input.replace(",", " ,");
+       // System.out.println("Input: " +input);
         String[] words = input.split(" ");
         String[] months = {"January","Febuary","March","April","May","June","July","August","September","October","November","December"};
+        
         for(int i=1;i<words.length;i++)
         {
             try
@@ -14,7 +23,19 @@ public static void main(String []args)
                 for(int j = 0; j < months.length; j++){
                     //System.out.println(words[i-1] + "   ,    " + months[j]);
                     if (words[i-1].equals(months[j])){
-                        System.out.println(a);
+                    	String meaningWords="";
+                    	timeline.add(months[j]+","+a);
+                    	for(int m=i+1;m<words.length;m++)
+                    	{
+                    		if(words[m].equals("."))
+                    			break;
+                    		
+                    		else
+                    			meaningWords+=words[m]+" ";
+                    	}
+                    	System.out.println(meaningWords);
+                    	//meanings.add(words)
+                       // System.out.println(months[j]+","+a);
                     }
                 }
                 
@@ -24,5 +45,13 @@ public static void main(String []args)
    
             }
         }
-        System.out.println(input);
+        System.out.println("Output:");
+        for(int i=0;i<timeline.size();i++)
+        {
+        	System.out.print("Date: "+timeline.get(i));
+        	System.out.println(" What happened: ");
+        }
+      
     }
+}
+
